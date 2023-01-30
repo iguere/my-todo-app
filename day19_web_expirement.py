1349242de457fc729a4516319736b3f8707d276c
ismaelguere <iguere06@gmail.com>
2023-01-30 17:16:49 -0500

    Add web configuration

--- day19_web_expirement.py
+++ day19_web_expirement.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide") # make horizontal length longer
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"  # provide the key of text widget
@@ -12,13 +13,21 @@ def add_todo():
     functions.write_todos(todos)
 
 
-
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+# st.write("<h1>This app is to increase your <b>productivity</b>.</h1>",
+#         unsafe_allow_html=True)
+# st.write("<h1>This app is to increase your <b>productivity</b>.</h1>",
+#          unsafe_allow_html=True) # <h1> will make it bold and increase font as well
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)  # this will bold the text by providing html <>; default is false
 
-st.checkbox("Buy grocery.")
-st.checkbox("Throw the trash.")
+# st.checkbox("Buy grocery.")
+# st.checkbox("Throw the trash.")
+
+# placement of this widget matter
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)  # store checkbox in a var
@@ -29,9 +38,6 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]  # delete the to-do from session state dictionary
         st.experimental_rerun()  # needed for checkbox/ serve as a refresher
-
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
 
 # print("Hi")
 #
